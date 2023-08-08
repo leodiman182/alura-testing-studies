0fe2b40b453a72190c7ddcafaf115c74d217907c
Leonardo Martins <leonardo.martins@cotefacil.com>
2023-08-08 17:51:53 -0300

    Finalizando aula 3

--- src/services/integracoes.test.js
+++ src/services/integracoes.test.js
@@ -1,56 +1,61 @@
 import { buscaTransacoes } from './transacoes';
 import api from './api';
-import { buscaSaldo } from './saldo';
+import { atualizaSaldo, buscaSaldo } from './saldo';
 
 jest.mock('./api');
 
-const mockTransacao = [
-  {
-    id: 1,
-    transacao: 'Depósito',
-    valor: '100',
-    data: '22/11/2022',
-    mes: 'Novembro',
-  },
-];
-
-const mockRequisicao = (retorno) => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      res({
-        data: retorno,
-      });
-    }, 200);
-  });
-};
-
-const mockRequisicaoErro = () => {
-  return new Promise((_, reject) => {
-    setTimeout(() => {
-      reject();
-    }, 200);
-  });
-};
-
-const mockRequisicaoSaldo = (retorno) => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      res({
-        data: {
-          valor: retorno,
-        },
-      });
-    });
-  });
-};
+// hook do próprio jest para executar uma ação em momento específico
+beforeEach(() => {
+  api.get.mockClear();
+});
 
 describe('Requisições para API', () => {
+  const mockTransacoes = [
+    {
+      id: 1,
+      transacao: 'Depósito',
+      valor: '100',
+      data: '22/11/2022',
+      mes: 'Novembro',
+    },
+  ];
+
+  const mockRequisicao = (retorno) => {
+    return new Promise((res) => {
+      setTimeout(() => {
+        res({
+          data: retorno,
+        });
+      }, 200);
+    });
+  };
+
+  const mockRequisicaoErro = () => {
+    return new Promise((_, reject) => {
+      setTimeout(() => {
+        reject();
+      }, 200);
+    });
+  };
+
+  const mockRequisicaoSaldo = (retorno) => {
+    return new Promise((res) => {
+      setTimeout(() => {
+        res({
+          data: {
+            valor: retorno,
+          },
+        });
+      });
+    });
+  };
+
   it('Deve retornar uma lista de transações', async () => {
-    api.get.mockImplementation(() => mockRequisicao(mockTransacao));
+    api.get.mockImplementation(() => mockRequisicao(mockTransacoes));
 
     const transacoes = await buscaTransacoes();
 
-    expect(transacoes).toEqual(mockTransacao);
+    expect(transacoes).toEqual(mockTransacoes);
     expect(api.get).toHaveBeenCalledWith('/transacoes');
   });
 
